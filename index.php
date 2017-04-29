bf5f06f37a662e89f718b1aadbee6e3d714c6b0c
ko22009 <ko22009@yandex.ru>
2017-04-29 12:29:34 +0500

    fix composer load

--- index.php
+++ index.php
@@ -6,12 +6,14 @@ if (!isset($_SESSION)) {
 
 $_SESSION['user_id'] = $_SERVER['HTTP_USER_ID']; // допустим, мы 2 пользователь
 
+require_once __DIR__ . '/vendor/autoload.php';
 ini_set('include_path', './app');
 
 if ($_SERVER['REQUEST_URI'] == '/') require_once 'page.php';
 else {
-    if(file_exists(__DIR__ . '/app' . $_SERVER['REQUEST_URI']))
-    require_once $_SERVER['REQUEST_URI'];
+	$path = __DIR__ . '/app' . $_SERVER['REQUEST_URI'];
+    if(file_exists($path))
+    require_once $path;
     else {
         header("HTTP/1.0 404 Not Found");
         echo 'This page was not found';
